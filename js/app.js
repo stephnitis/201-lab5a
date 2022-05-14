4916bb8445478def21c0d6b01744304ff25755d6
Stephanie Hill <stephni.hill@gmail.com>
2022-05-13 19:27:31 -0700

    problemFour solved

--- js/app.js
+++ js/app.js
@@ -1,69 +1,69 @@
-// 'use strict';
-// /////////////////////////////////////
-// /* Problem 1 (this is your demo that we'll solve in class)
-// Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+// // 'use strict';
+// // /////////////////////////////////////
+// // /* Problem 1 (this is your demo that we'll solve in class)
+// // Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
-// "The sum of 4 and 7 is 11."
+// // "The sum of 4 and 7 is 11."
 
-// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+// // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// // Write your code here
+// // // Write your code here
 function sum(a, b) { //eslint-disable-line
   let mySum = a + b;
   let myStr = `The sum of ${a} and ${b} is ${mySum}.`;
   return [mySum, myStr];
 }
 
-// // Here is the test for sum(); uncomment it to run it
-// //testSum(4, 7);
+// // // Here is the test for sum(); uncomment it to run it
+// // //testSum(4, 7);
 
-// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-// /////////////////////////////////////
-// /* Problem 2
-// Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+// // /////////////////////////////////////
+// // /* Problem 2
+// // Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-// "The product of 5 and 9 is 45."
+// // "The product of 5 and 9 is 45."
 
-// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+// // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// // Write your code here
+// // // Write your code here
 function multiply(a, b) { //eslint-disable-line
   let myProduct = a * b;
   let myStrTwo = `The product of ${a} and ${b} is ${myProduct}.`;
 
   return [myProduct, myStrTwo];
 }
-//   // // Here is the test for multiply(); uncomment it to run it
-//   testMultiply(5, 9);
+// //   // // Here is the test for multiply(); uncomment it to run it
+// //   testMultiply(5, 9);
 
-// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-// /////////////////////////////////////
-// /* Problem 3
-// Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+// // /////////////////////////////////////
+// // /* Problem 3
+// // Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-// Third element: "4 and 7 and 5 sum to 16."
-// Fourth element: "The product of 4 and 7 and 5 is 140."
+// // Third element: "4 and 7 and 5 sum to 16."
+// // Fourth element: "The product of 4 and 7 and 5 is 140."
 
-// IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+// // IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+// // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let sumThree = sum(a,b)[0];
-  let finalSumThree = sum(sumThree, c)[0];
-  let thirdStr = `${a} and ${b} and ${c} sum to ${finalSumThree}.`;
-  let productFour = multiply(a,b)[0];
-  let finalProductFour = multiply(productFour, c)[0];
-  let fourthStr = `The product of ${a} and ${b} and ${c} is ${finalProductFour}.`;
+// // // Write your code here
+// function sumAndMultiply(a, b, c) { //eslint-disable-line
+//   let sumThree = sum(a, b)[0];
+//   let finalSumThree = sum(sumThree, c)[0];
+//   let thirdStr = `${a} and ${b} and ${c} sum to ${finalSumThree}.`;
+//   let productFour = multiply(a, b)[0];
+//   let finalProductFour = multiply(productFour, c)[0];
+//   let fourthStr = `The product of ${a} and ${b} and ${c} is ${finalProductFour}.`;
 
-  return [finalSumThree, finalProductFour, thirdStr, fourthStr];
-}
+//   return [finalSumThree, finalProductFour, thirdStr, fourthStr];
+// }
 
-// // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4, 7, 5);
+// // // Here is the test for sumAndMultiply(); uncomment it to run it
+// testSumAndMultiply(4, 7, 5);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -78,15 +78,18 @@ testSumAndMultiply(4, 7, 5);
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // // Write your code here
-// let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; //eslint-disable-line
 
-// function sumArray(sumArr) { //eslint-disable-line
+function sumArray(array) { 
+  let sumFive = sum(array[0], sum(array[1], array[2])[0])[0];
+  let StrFive = `${array} was passed in as an array of numbers, and ${sumFive} is their sum.`;
 
-// }
+return [sumFive, StrFive];
+}
 
 // // Here is the test for sumArray(); uncomment it to run it
 
-// // testSumArray(testArray);
+testSumArray(testArray);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
